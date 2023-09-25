2765648120a48925390e366438e7b43a0173060f
orabillon <115501074+orabillon@users.noreply.github.com>
2023-09-25 13:35:19 +0200

    Création Template

---
+++ MyTemplate/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new MyTemplate.MainGame();
+game.Run();
---
+++ MyTemplate/Template/GameState.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MyTemplate;
+
+public class GameState
+{
+    public GameState()
+    {
+        Console.WriteLine("debug");
+    }
+}
