e7d007abe13d395f53286791daa0181c887bfa29
orabillon <115501074+orabillon@users.noreply.github.com>
2024-03-31 16:34:07 +0200

    fin lunar tuto

--- Lunar Lander/Lunar Lander/Game1.cs
+++ Lunar Lander/Lunar Lander/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace Lunar_Lander
 {
@@ -8,6 +9,8 @@ namespace Lunar_Lander
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        Lander lander;
 
         public Game1()
         {
@@ -28,6 +31,13 @@ namespace Lunar_Lander
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            lander = new Lander();
+
+            lander.img = Content.Load<Texture2D>("ship");
+            lander.imgEngine = Content.Load<Texture2D>("engine");
+
+            lander.position = new Vector2(GraphicsDevice.Viewport.Width / 2,
+                                        GraphicsDevice.Viewport.Height / 2);
         }
 
         protected override void Update(GameTime gameTime)
@@ -36,15 +46,63 @@ namespace Lunar_Lander
                 Exit();
 
             // TODO: Add your update logic here
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                lander.angle += 2;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                lander.angle -= 2;
+            }
+            // Allumage du moteur
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                lander.engineOn = true;
+
+                float angle_radian = MathHelper.ToRadians(lander.angle);
+                float force_x = (float)Math.Cos(angle_radian) * lander.speed;
+                float force_y = (float)Math.Sin(angle_radian) * lander.speed;
+                lander.velocity += new Vector2(force_x, force_y);
+            }
+            else
+            {
+                lander.engineOn = false;
+            }
+
+            lander.update();
+
+            if (lander.position.X < 0)
+            {
+                lander.position = new Vector2(GraphicsDevice.Viewport.Width, lander.position.Y);
+            }
+            if (lander.position.X > GraphicsDevice.Viewport.Width)
+            {
+                lander.position = new Vector2(GraphicsDevice.Viewport.Width, lander.position.Y);
+                lander.velocity = new Vector2(0 - lander.velocity.X, lander.velocity.Y);
+            }
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            Vector2 originImg = new Vector2(lander.img.Width / 2, lander.img.Height / 2);
+            _spriteBatch.Draw(lander.img, lander.position, null, Color.White,
+                MathHelper.ToRadians(lander.angle), originImg, new Vector2(1, 1), SpriteEffects.None, 0);
+
+            if (lander.engineOn)
+            {
+                Vector2 originImgEngine = new Vector2(lander.imgEngine.Width / 2, lander.imgEngine.Height / 2);
+                _spriteBatch.Draw(lander.imgEngine, lander.position, null, Color.White,
+                    MathHelper.ToRadians(lander.angle), originImgEngine, new Vector2(1, 1), SpriteEffects.None, 0);
+            }
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
---
+++ Lunar Lander/Lunar Lander/Lander.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lunar_Lander
+{
+    internal class Lander
+    {
+        public Vector2 position { get; set; } = Vector2.Zero;
+        public Vector2 velocity { get; set; } = Vector2.Zero;
+        public float angle { get; set; } = 270;
+        public bool engineOn { get; set; } = false;
+        public float speed { get; set; } = 0.02f;
+        private float speedMax = 2f;
+        public Texture2D img { get; set; }
+        public Texture2D imgEngine { get; set; }
+
+        public void update()
+        {
+            velocity += new Vector2(0, 0.005f);
+
+            if (Math.Abs(velocity.X) >= speedMax)
+            {
+                velocity = new Vector2((velocity.X < 0 ? 0 - speedMax : speedMax), velocity.Y);
+            }
+            if (Math.Abs(velocity.Y) >= speedMax)
+            {
+                velocity = new Vector2(velocity.X, (velocity.Y < 0 ? 0 - speedMax : speedMax));
+            }
+
+            position += velocity;
+        }
+
+    }
+}
