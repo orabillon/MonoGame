fbf7f02486403594f912687de816a8d1f748bac0
orabillon <115501074+orabillon@users.noreply.github.com>
2024-04-07 15:57:24 +0200

    Compilation pour integration dans jeu

---
+++ Tetris/Tetris/MainGame.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using ornLib.src;
+
+namespace Tetris
+{
+    public class MainGame : Engine
+    {
+        
+        public MainGame() : base() { }
+       
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+           
+            base.LoadContent();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+           
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Tetris/Tetris/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new Tetris.MainGame();
+game.Run();
